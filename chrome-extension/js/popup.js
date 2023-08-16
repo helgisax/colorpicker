19039cf2e2b0d2358c22d345cfbf188c79eb1fd0
helgisax <helgiingvarsson77@gmail.com>
2023-08-16 16:31:26 +0200

    optim

--- chrome-extension/js/popup.js
+++ chrome-extension/js/popup.js
@@ -1,41 +1,52 @@
-document.addEventListener('DOMContentLoaded', () => {
-  chrome.tabs.executeScript({
-    code: 'window.showColorPicker = true;'
-});
-  chrome.tabs.executeScript({
-      file: 'contentScript.js'
-  });
-  
-  displayRecentColors();
-});
+const rgbToHex = (rgb) => {
+  const result = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/.exec(rgb);
+  return result ? `#${parseInt(result[1]).toString(16).padStart(2, '0')}${parseInt(result[2]).toString(16).padStart(2, '0')}${parseInt(result[3]).toString(16).padStart(2, '0')}`.toUpperCase() : null;
+};
 
-const fetchColorFromPage = () => {
-  document.body.addEventListener('click', (e) => {
-      const color = window.getComputedStyle(e.target).backgroundColor;
-      chrome.runtime.sendMessage(color);
-      document.body.style.cursor = '';
-  }, { once: true });
-}
+const rgbToHsl = (r, g, b) => {
+  r /= 255, g /= 255, b /= 255;
+  const max = Math.max(r, g, b), min = Math.min(r, g, b);
+  let h, s, l = (max + min) / 2;
+
+  if (max == min) {
+      h = s = 0; 
+  } else {
+      const delta = max - min;
+      s = l > 0.5 ? delta / (2 - max - min) : delta / (max + min);
+      switch (max) {
+          case r: h = (g - b) / delta + (g < b ? 6 : 0); break;
+          case g: h = (b - r) / delta + 2; break;
+          case b: h = (r - g) / delta + 4; break;
+      }
+      h /= 6;
+  }
+  return `hsl(${(h * 360).toFixed(0)}, ${(s * 100).toFixed(1)}%, ${(l * 100).toFixed(1)}%)`;
+};
+
+const extractRGB = (rgbString) => {
+  const result = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/.exec(rgbString);
+  if (result) {
+      return {
+          r: parseInt(result[1]),
+          g: parseInt(result[2]),
+          b: parseInt(result[3])
+      };
+  }
+  return null;
+};
+
 
 const storeColor = (color) => {
-  
   let colors = JSON.parse(localStorage.getItem('recentColors') || '[]');
+  
   colors.unshift(color);
-  if (colors.length > 11) {
+  if (colors.length > 10) {
       colors.pop();
   }
   localStorage.setItem('recentColors', JSON.stringify(colors));
-
-  const recentColorsSection = document.getElementById('recentColors');
-  recentColorsSection.innerHTML = '';
-
-  colors.forEach(storedColor => {
-      const colorDiv = document.createElement('div');
-      colorDiv.classList.add('color-circle'); 
-      colorDiv.style.backgroundColor = storedColor;
-      recentColorsSection.appendChild(colorDiv);
-  });
+  displayRecentColors();
 }
+
 
 const displayRecentColors = () => {
   const recentColorsSection = document.getElementById('recentColors');
@@ -51,6 +62,35 @@ const displayRecentColors = () => {
       recentColorsSection.appendChild(colorDiv);
   });
 }
+
+
+const fetchColorFromPage = () => {
+  document.body.addEventListener('click', (e) => {
+      const color = window.getComputedStyle(e.target).backgroundColor;
+      chrome.runtime.sendMessage(color);
+      document.body.style.cursor = '';
+  }, { once: true });
+}
+
+
+const clearRecentColors = () => {
+  const recentColorsContainer = document.getElementById('recentColors');
+  recentColorsContainer.innerHTML = '';
+  localStorage.removeItem('recentColors');
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.tabs.executeScript({
+    code: 'window.showColorPicker = true;'
+});
+  chrome.tabs.executeScript({
+      file: 'contentScript.js'
+  });
+  
+  displayRecentColors();
+});
+
 
 chrome.runtime.onMessage.addListener(color => {
   const hexCodeUpper = document.getElementById('hexCodeUpper');
@@ -71,50 +111,4 @@ chrome.runtime.onMessage.addListener(color => {
   storeColor(color);
 });
 
-const rgbToHex = (rgb) => {
-  const result = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/.exec(rgb);
-  return result ? `#${parseInt(result[1]).toString(16).padStart(2, '0')}${parseInt(result[2]).toString(16).padStart(2, '0')}${parseInt(result[3]).toString(16).padStart(2, '0')}`.toUpperCase() : null;
-}
-
-const rgbToHsl = (r, g, b) => {
-  r /= 255, g /= 255, b /= 255;
-  const max = Math.max(r, g, b), min = Math.min(r, g, b);
-  let h, s, l = (max + min) / 2;
-
-  if (max == min) {
-      h = s = 0; 
-  } else {
-      const delta = max - min;
-      s = l > 0.5 ? delta / (2 - max - min) : delta / (max + min);
-      switch (max) {
-          case r: h = (g - b) / delta + (g < b ? 6 : 0); break;
-          case g: h = (b - r) / delta + 2; break;
-          case b: h = (r - g) / delta + 4; break;
-      }
-      h /= 6;
-  }
-
-  return `hsl(${(h * 360).toFixed(0)}, ${(s * 100).toFixed(1)}%, ${(l * 100).toFixed(1)}%)`;
-}
-
-const extractRGB = (rgbString) => {
-  const result = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/.exec(rgbString);
-  if (result) {
-      return {
-          r: parseInt(result[1]),
-          g: parseInt(result[2]),
-          b: parseInt(result[3])
-      };
-  }
-  return null;
-}
-
-const clearRecentColors = () => {
-  const recentColorsContainer = document.getElementById('recentColors');
-  recentColorsContainer.innerHTML = '';
-  localStorage.removeItem('recentColors');
-}
-
 document.getElementById('clear').addEventListener('click', clearRecentColors);
-
-
